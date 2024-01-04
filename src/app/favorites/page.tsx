a40c092cb5d92cf59792b390521053de38cb591e
Miguel Angel Jimenez <koekun3000@gmail.com>
2024-01-03 23:37:03 -0500

    #FIX: Fixing height of favorites section

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -10,26 +10,28 @@ export default function AnimeFavorites() {
 
   return (
     <Wrapper>
-      <article className='flex items-center gap-4'>
-        <span className='text-red-500'>
-          <IconHeartFilled size={32} />
-        </span>
-        <h2 className='text-4xl font-bold'>Favorites</h2>
-      </article>
-      <div className='w-full min-h-[calc(100vh_-_380px)] '>
+      <div className='relative w-full min-h-[calc(100vh_-_380px)] '>
         {favAnimes !== undefined &&
           (favAnimes.length === 0 ? (
-            <article className='w-full grid place-content-center'>
+            <article className='absolute w-full h-full grid place-content-center'>
               <h2 className='text-6xl font-bold'>
                 Your favorite list are empty
               </h2>
             </article>
           ) : (
-            <ul className='grid grid-cols-6 gap-4'>
-              {favAnimes.map((item) => {
-                return <CardAnime key={item.mal_id} item={item} />
-              })}
-            </ul>
+            <>
+              <article className='flex items-center gap-4'>
+                <span className='text-red-500'>
+                  <IconHeartFilled size={32} />
+                </span>
+                <h2 className='text-4xl font-bold'>Favorites</h2>
+              </article>
+              <ul className='grid grid-cols-6 gap-4'>
+                {favAnimes.map((item) => {
+                  return <CardAnime key={item.mal_id} item={item} />
+                })}
+              </ul>
+            </>
           ))}
       </div>
     </Wrapper>
