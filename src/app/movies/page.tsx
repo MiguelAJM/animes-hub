e259c09c477a71fb3a4cd777d1603f7cc23bb14b
Miguel Angel Jimenez <koekun3000@gmail.com>
2024-01-04 13:58:42 -0500

    #ADD: Movies UI & page

---
+++ src/app/movies/page.tsx
@@ -0,0 +1,26 @@
+import { getMovies } from '@/utils/services/getMovies'
+import { IconPlayerPlay } from '@tabler/icons-react'
+import Wrapper from '@/components/Wrapper'
+import MainTitleAnime from '@/components/anime/MainTitleAnime'
+import CardAnime from '@/components/characters/CardAnime'
+
+export default async function MoviesAnime() {
+  const movies = await getMovies()
+  const { data } = movies
+
+  return (
+    <Wrapper>
+      <MainTitleAnime
+        title='Animes'
+        IconTitle={<IconPlayerPlay size={32} />}
+        iconColor='text-magenta-500'
+        button={false}
+      />
+      <ul className='grid grid-cols-6 gap-4'>
+        {data.map((item) => (
+          <CardAnime key={item.mal_id} item={item} />
+        ))}
+      </ul>
+    </Wrapper>
+  )
+}
--- src/components/navbars/NavLinks.tsx
+++ src/components/navbars/NavLinks.tsx
@@ -1,5 +1,10 @@
 'use client'
-import { IconCategory, IconHeart, IconHome } from '@tabler/icons-react'
+import {
+  IconCategory,
+  IconHeart,
+  IconHome,
+  IconPlayerPlay
+} from '@tabler/icons-react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
@@ -9,17 +14,22 @@ export default function NavLinks() {
   const links = [
     { url: '/', name: 'Home', icon: IconHome },
     { url: '/genres', name: 'Genres', icon: IconCategory },
+    { url: '/movies', name: 'Movies', icon: IconPlayerPlay },
     { url: '/favorites', name: 'Favorites', icon: IconHeart }
   ]
 
   return (
     <ul className='flex flex-col gap-3'>
       {links.map((item) => (
-        <Link className={`${
-          pathname === item.url
-            ? 'bg-light text-darkest'
-            : 'bg-darkest text-lightest'
-        } p-2 lg:p-3 rounded-xl`} key={item.name} href={item.url}>
+        <Link
+          className={`${
+            pathname === item.url
+              ? 'bg-light text-darkest'
+              : 'bg-darkest text-lightest'
+          } p-2 lg:p-3 rounded-xl`}
+          key={item.name}
+          href={item.url}
+        >
           <item.icon size={24} />
         </Link>
       ))}
