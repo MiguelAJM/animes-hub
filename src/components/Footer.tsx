702647dd8daa3cfee2d1bd7818510ff74bb6eb9f
Miguel Angel Jimenez <koekun3000@gmail.com>
2024-01-04 01:07:55 -0500

    #DELETE: Old footer

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className='relative flex container mx-auto transition-all duration-300 ease-in-out'>
-      <h1>Footer</h1>
-    </footer>
-  )
-}
